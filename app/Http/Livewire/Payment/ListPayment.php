a7aa5f4a75c970a66ea2cd00bf01bd986a76a373
Abel <abel291m@gmail.com>
2022-04-15 07:27:25 -0500

    crud payments

---
+++ app/Http/Livewire/Payment/ListPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Payment;
+
+use Livewire\Component;
+
+class ListPayment extends Component
+{
+    public function render()
+    {
+        return view('livewire.payment.list-payment');
+    }
+}
