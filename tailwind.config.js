00495087441c7b2c41a4d494a8a129bebf880571
Abel <abel291m@gmail.com>
2022-05-24 05:31:38 -0500

    change bd and create page checkout

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,12 @@ module.exports = {
         container: {
             center: true,
             padding: {
-                DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "6rem",
-            },
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+              },
         },
 
         colors: {
