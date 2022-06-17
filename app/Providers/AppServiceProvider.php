4a80fd9cabc61105a9b6b2099cba05430f9e002c
Abel <abel291m@gmail.com>
2022-06-17 02:03:03 -0500

    heroku fix http - https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
