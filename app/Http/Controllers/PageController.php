c083dd527ee10a0a3bfe5ace2cde5963f507a314
Abel Morales <abel291m@gmail.com>
2022-07-27 05:02:57 -0500

    fix home method random() collection

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,41 +11,41 @@ use Inertia\Inertia;
 
 class PageController extends Controller
 {
-    public function home()
-    {
-        $carousel = Event::where('home', true)->has('session')->get()->random(10);
+	public function home()
+	{
+		$carousel = Event::where('home', true)->has('session')->get()->take(10);
 
-        $featured = Event::has('session')->with('session', 'category', 'location')->get()->random(8);
+		$featured = Event::has('session')->with('session', 'category', 'location')->get()->take(8);
 
-        $free = Event::has('session')->with('session', 'category', 'location')->whereRelation('ticket_types', 'type', 'free')->get()->random(8);
+		$free = Event::has('session')->with('session', 'category', 'location')->whereRelation('ticket_types', 'type', 'free')->get()->take(8);
 
-        $home_categories = Category::where('type', CategoryType::EVENT)->get();
+		$home_categories = Category::where('type', CategoryType::EVENT)->get();
 
-        return Inertia::render('Home/Home', [
-            'eventsCarousel' => EventResource::collection($carousel),
-            'eventsFeacture' => EventResource::collection($featured),
-            'eventsFree' => EventResource::collection($free),
-            'categories' => CategoryResource::collection($home_categories),
-        ]);
-    }
+		return Inertia::render('Home/Home', [
+			'eventsCarousel' => EventResource::collection($carousel),
+			'eventsFeacture' => EventResource::collection($featured),
+			'eventsFree' => EventResource::collection($free),
+			'categories' => CategoryResource::collection($home_categories),
+		]);
+	}
 
-    public function privacy_policy()
-    {
-        return Inertia::render('PrivacyPolicy/PrivacyPolicy');
-    }
+	public function privacy_policy()
+	{
+		return Inertia::render('PrivacyPolicy/PrivacyPolicy');
+	}
 
-    public function about_us()
-    {
-        return Inertia::render('AboutUs/AboutUs');
-    }
+	public function about_us()
+	{
+		return Inertia::render('AboutUs/AboutUs');
+	}
 
-    public function terms_of_service()
-    {
-        return Inertia::render('TermsOfService/TermsOfService');
-    }
+	public function terms_of_service()
+	{
+		return Inertia::render('TermsOfService/TermsOfService');
+	}
 
-    public function faq()
-    {
-        return Inertia::render('Faq/Faq');
-    }
+	public function faq()
+	{
+		return Inertia::render('Faq/Faq');
+	}
 }
